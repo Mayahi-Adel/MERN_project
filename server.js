0fac132a714ce803203049c160e2bfea407610a1
Mayahi Adel <adel.mayahii@gmail.com>
2021-11-03 21:54:05 +0100

    init project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+require("./config/db");
+
+dotenv.config({ path: "./config/.env" });
+
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to my API");
+// });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
