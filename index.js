040bb33a32a195c9e18ace3436feb86de7e0e409
Mayahi Adel <adel.mayahii@gmail.com>
2022-01-05 22:10:28 +0100

    deploy preparatio,n

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+require("./config/db");
+
+dotenv.config({ path: "./config/.env" });
+
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+
+const app = express();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//JWT
+app.get("*", checkUser);
+app.get("/api/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello to raccoon API");
+});
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
