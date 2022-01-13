da06880c13e6b5f995771380c449150d2ed9dd21
Mayahi Adel <adel.mayahii@gmail.com>
2022-01-13 23:52:11 +0100

    style logo

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ if (process.env.NODE_ENV === "production") {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} 🚀`);
 });
